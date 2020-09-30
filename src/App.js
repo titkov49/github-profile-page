3b62021e0dc49e2c3038be6ca395b268ce1f7dcd
Dzmitry Tsitkou <titkov49@gmail.com>
2020-09-30 20:51:40 +0200

    add Error page

--- src/App.js
+++ src/App.js
@@ -2,28 +2,34 @@ import React, { useState, useEffect } from 'react';
 import Input from './components/Input';
 import Bio from './components/Bio';
 import Repositories from './components/Repositories';
+import Error from './components/Error';
 import './App.scss';
 
 function App() {
   const [username, setUsername] = useState('');
   const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
 
   useEffect(() => {
     if (!username) return;
     fetch(`https://api.github.com/users/${username}`)
-      .then(res => res.json())
-      .then(data => setUser(data));
+      .then(res => {
+        if (res.status === 404) throw Error(res.statusText);
+        setErr(false)
+        return res.json();
+      })
+      .then(data => setUser(data))
+      .catch(e => {
+        setErr(true);
+        setUser(null);
+      });
   }, [username]);
-
-  useEffect(()=>{
-    if(!user) return;
-    console.log("App.js PROFILE", user)
-  }, [user])
 
   return (
     <div className="App">
       <Input setValue={setUsername} placeholder="Type username" />
-      {user &&
+      {err && <Error />}
+      {user && !err &&
         <div id="profile">
           <Bio user={user} />
           <Repositories user={user} />
