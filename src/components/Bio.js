55f784a5c0e0a465291d487266b46381ce5dfc2c
Dzmitry Tsitkou <titkov49@gmail.com>
2020-09-30 19:17:19 +0200

    add styles to bio, change html structure in bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,9 +3,14 @@ import React from 'react';
 export default function ({ user }) {
   return (
     <div id="bio">
-      <img src={user.avatar_url} alt="avatar" />
-      <h3>{user.name}</h3>
-      <p>{user.login}</p>
+      <div className="main-info">
+        <img src={user.avatar_url} alt="avatar" />
+        <div className="names">
+          <h1>{user.name}</h1>
+          <p>{user.login}</p>
+        </div>
+      </div>
+      <p>{user.bio}</p>
       <a href={`${user.html_url}?tab=repositories`}><button>Go to github</button></a>
       <p>{user.followers} followers | {user.following} following</p>
       <p>{user.location}</p>
