ef8236cd7d0187c77cdf09759dc08b85757cdb74
Dzmitry Tsitkou <titkov49@gmail.com>
2020-11-02 16:44:04 +0100

    Updates

---
+++ precache-manifest.575cb31fd70e66e917e7526a15f8b5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a6e19689a11ab538c574cb008cfadf",
+    "url": "/github-profile-page/index.html"
+  },
+  {
+    "revision": "0e23aabade14f8bc00cc",
+    "url": "/github-profile-page/static/css/main.b8108d01.chunk.css"
+  },
+  {
+    "revision": "5e803e51bb2fc81d500b",
+    "url": "/github-profile-page/static/js/2.3f306576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profile-page/static/js/2.3f306576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e23aabade14f8bc00cc",
+    "url": "/github-profile-page/static/js/main.781a6aa9.chunk.js"
+  },
+  {
+    "revision": "7fc3d8acade283ed55a7",
+    "url": "/github-profile-page/static/js/runtime-main.211feedb.js"
+  }
+]);
